609f316ed8d9a2d69d0cfd326c0a753316667b54
Donia Chaiehloudj <donia.chaiehloudj.external@airbus.com>
2020-07-31 17:44:58 +0200

    Lesson-7 empty brackets exercice

---
+++ 7-stacksandqueues/brackets/main.go
@@ -0,0 +1,25 @@
+package brackets
+
+const (
+	bracketOpen  = "("
+	bracketClose = ")"
+	hookOpen     = "["
+	hookClose    = "]"
+	curlyOpen    = "{"
+	curlyClose   = "}"
+)
+
+// Brackets returns 1 if the brackets are correctly nested otherwise 0
+func Brackets(S string) int {
+	var isOk int
+	/*bracket := make([]string, 0)
+	hook := make([]string, 0)
+	curly := make([]string, 0)
+
+	a := strings.Split(S, "")
+	for i := range a {
+
+	}
+	*/
+	return isOk
+}
